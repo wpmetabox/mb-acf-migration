9545bc92ee3f9fc15239130a48e46d1ae6d41967
Anh Tran <anhtnt@elightup.com>
2021-06-07 09:46:54 +0700

    First commit

---
+++ mb-acf-migration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: MB ACF Migration
+ * Plugin URI:  https://metabox.io
+ * Description: Migrate ACF custom fields to Meta Box.
+ * Version:     0.1.0
+ * Author:      MetaBox.io
+ * Author URI:  https://metabox.io
+ * License:     GPL2+
+ * Text Domain: meta-box
+ * Domain Path: /languages/
+ */
+
+defined( 'ABSPATH' ) || die;
