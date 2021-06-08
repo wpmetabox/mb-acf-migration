d44ebd31ffcda6f18f4e533337ce6bc714cec125
Anh Tran <anhtnt@elightup.com>
2021-06-08 17:55:54 +0700

    Done migrating editor

--- src/Modules/FieldType.php
+++ src/Modules/FieldType.php
@@ -29,9 +29,74 @@ class FieldType {
 
 		$this->settings['_id']    = $this->settings['type'] . '_' . uniqid();
 		$this->settings['_state'] = 'collapse';
+
+		unset( $this->settings['wrapper'] );
 	}
 
 	private function migrate_image() {
 		$this->settings['type'] = 'single_image';
+		Arr::change_key( $this->settings, 'preview_size', 'image_size' );
+
+		unset( $this->settings['return_format'] );
+		unset( $this->settings['library'] );
+		unset( $this->settings['min_width'] );
+		unset( $this->settings['min_height'] );
+		unset( $this->settings['min_size'] );
+		unset( $this->settings['max_width'] );
+		unset( $this->settings['max_height'] );
+		unset( $this->settings['max_size'] );
+		unset( $this->settings['mime_types'] );
+	}
+
+	private function migrate_file() {
+		$this->settings['type'] = 'file_advanced';
+		$this->settings['max_file_uploads'] = 1;
+
+		unset( $this->settings['return_format'] );
+		unset( $this->settings['library'] );
+		unset( $this->settings['min_size'] );
+		unset( $this->settings['max_size'] );
+		unset( $this->settings['mime_types'] );
+	}
+
+	private function migrate_wysiwyg() {
+		$options = [];
+		if ( $this->settings['toolbar'] === 'basic' ) {
+			$id = uniqid();
+			$options[ $id ] = [
+				'id'    => $id,
+				'key'   => 'teeny',
+				'value' => true,
+			];
+		}
+		if ( $this->settings['tabs'] === 'visual' ) {
+			$id = uniqid();
+			$options[ $id ] = [
+				'id'    => $id,
+				'key'   => 'quicktags',
+				'value' => false,
+			];
+		}
+		if ( $this->settings['tabs'] === 'text' ) {
+			$id = uniqid();
+			$options[ $id ] = [
+				'id'    => $id,
+				'key'   => 'tinymce',
+				'value' => false,
+			];
+		}
+
+		$id = uniqid();
+		$options[ $id ] = [
+			'id'    => $id,
+			'key'   => 'media_buttons',
+			'value' => (bool) $this->settings['media_upload'],
+		];
+
+		$this->settings['options'] = $options;
+
+		unset( $this->settings['toolbar'] );
+		unset( $this->settings['media_upload'] );
+		unset( $this->settings['delay'] );
 	}
 }
