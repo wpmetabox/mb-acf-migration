39d445ab9d8f9ede0014649fd0f2bd4cb8303f72
Anh Tran <anhtnt@elightup.com>
2021-06-11 09:12:18 +0700

    Add some notes for migration

--- src/AdminPage.php
+++ src/AdminPage.php
@@ -20,7 +20,8 @@ class AdminPage {
 	}
 
 	public function enqueue() {
-		wp_enqueue_script( 'mb-acf', plugins_url( 'js/migrate.js', __DIR__ ), [], '', true );
+		wp_enqueue_style( 'mb-acf', plugins_url( 'assets/migrate.css', __DIR__ ), [], '1.0.0' );
+		wp_enqueue_script( 'mb-acf', plugins_url( 'assets/migrate.js', __DIR__ ), [], '1.0.0', true );
 		wp_localize_script( 'mb-acf', 'MbAcf', [
 			'start'                  => __( 'Start', 'mb-acf-migration' ),
 			'done'                   => __( 'Done', 'mb-acf-migration' ),
@@ -36,7 +37,25 @@ class AdminPage {
 		?>
 		<div class="wrap">
 			<h1><?= get_admin_page_title() ?></h1>
-			<button class="button button-primary" id="process"><?php esc_html_e( 'Migrate', 'mb-acf-migration' ) ?></button>
+			<p>
+				<button class="button button-primary" id="process"><?php esc_html_e( 'Migrate', 'mb-acf-migration' ) ?></button>
+			</p>
+			<h2><?php esc_html_e( 'Notes:', 'mb-acf-migration' ) ?></h2>
+			<ul>
+				<li><?php esc_html_e( 'Always backup your database first as the plugin will remove/replace the existing ACF data. If you find any problem, restore the database and report us. We can\'t help you if you don\'t backup the database and there\'s something wrong.', 'mb-acf-migration' ) ?></li>
+				<li><?php esc_html_e( 'Not all data types and settings in ACF have an equivalent in Meta Box. The plugin will try to migrate as much as it can. But for such data or settings, the plugin will ignore. This includes: link, accordion, clone field types and complex location/conditional logic rules.', 'mb-acf-integration' ) ?></li>
+				<li>
+					<?php esc_html_e( 'The plugin will attempt to migrate both field groups and custom fields data to Meta Box. You might need some premium extensions like Meta Box Builder for all the data type, such as:', 'mb-acf-migration' ) ?>
+					<ul>
+						<li><a href="https://metabox.io/plugins/meta-box-builder/" target="_blank">Meta Box Builder</a>: <?php esc_html_e( 'For managing field groups settings.', 'mb-acf-migration' ) ?></li>
+						<li><a href="https://metabox.io/plugins/mb-term-meta/" target="_blank">MB Term Meta</a>: <?php esc_html_e( 'For handling custom fields for taxonomies.', 'mb-acf-migration' ) ?></li>
+						<li><a href="https://metabox.io/plugins/mb-user-meta/" target="_blank">MB User Meta</a>: <?php esc_html_e( 'For handling custom fields for users.', 'mb-acf-migration' ) ?></li>
+						<li><a href="https://metabox.io/plugins/mb-settings-pages/" target="_blank">MB Settings Pages</a>: <?php esc_html_e( 'For handling custom fields for settings pages.', 'mb-acf-migration' ) ?></li>
+						<li><a href="https://metabox.io/plugins/mb-blocks/" target="_blank">MB Blocks</a>: <?php esc_html_e( 'For handling custom fields for blocks.', 'mb-acf-migration' ) ?></li>
+					</ul>
+				</li>
+				<li><?php esc_html_e( 'You need to update the code to output fields data on the frontend to make it work with Meta Box.', 'mb-acf-integration' ) ?></li>
+			</ul>
 			<div id="status"></div>
 		</div>
 		<?php
