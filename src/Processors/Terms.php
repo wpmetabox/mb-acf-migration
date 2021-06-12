103aa7d6ae881140f3cc36ea3db72942d5d843f7
Anh Tran <anhtnt@elightup.com>
2021-06-12 16:21:15 +0700

    Migrate terms and users

--- src/Processors/Terms.php
+++ src/Processors/Terms.php
@@ -1,12 +1,42 @@
 <?php
 namespace MetaBox\ACF\Processors;
 
+use MetaBox\Support\Data as Helper;
+
 class Terms extends Base {
 	protected function get_items() {
+		$terms = get_terms( [
+			'taxonomy'   => array_keys( Helper::get_taxonomies() ),
+			'hide_empty' => false,
+			'number'     => $this->threshold,
+			'offset'     => $_SESSION['processed'],
+		] );
 
+		return $terms;
 	}
 
 	protected function migrate_item() {
+		$this->migrate_fields();
+	}
 
+	private function migrate_fields() {
+		$fields = new Data\Fields( 0, $this );
+		$fields->migrate_fields();
+	}
+
+	public function get( $key ) {
+		return get_term_meta( $this->item->ID, $key, true );
+	}
+
+	public function add( $key, $value ) {
+		add_term_meta( $this->item->ID, $key, $value, false );
+	}
+
+	public function update( $key, $value ) {
+		update_term_meta( $this->item->ID, $key, $value );
+	}
+
+	public function delete( $key ) {
+		delete_term_meta( $this->item->ID, $key );
 	}
 }
--- src/Processors/Users.php
+++ src/Processors/Users.php
@@ -3,10 +3,37 @@ namespace MetaBox\ACF\Processors;
 
 class Users extends Base {
 	protected function get_items() {
+		$users = get_users( [
+			'number'      => $this->threshold,
+			'offset'      => $_SESSION['processed'],
+			'count_total' => false,
+		] );
 
+		return $users;
 	}
 
 	protected function migrate_item() {
+		$this->migrate_fields();
+	}
 
+	private function migrate_fields() {
+		$fields = new Data\Fields( 0, $this );
+		$fields->migrate_fields();
+	}
+
+	public function get( $key ) {
+		return get_user_meta( $this->item->ID, $key, true );
+	}
+
+	public function add( $key, $value ) {
+		add_user_meta( $this->item->ID, $key, $value, false );
+	}
+
+	public function update( $key, $value ) {
+		update_user_meta( $this->item->ID, $key, $value );
+	}
+
+	public function delete( $key ) {
+		delete_user_meta( $this->item->ID, $key );
 	}
 }
